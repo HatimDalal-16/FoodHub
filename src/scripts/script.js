f253514b3c3c26032dc73c6a05f28902ce5ff9fa
Hatim <dalalhatim1316@gmail.com>
2026-02-27 08:33:36 +0530

    service-card-updated

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -81,3 +81,37 @@ gsap.to(".vision-left", {
     scrub: 1,
   }
 });
+
+// Initialize Slick Slider for Service Cards
+$(document).ready(function(){
+    $('.service-slider').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 9000,
+        cssEase: 'linear',
+        arrows: false,
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        draggable: true,
+        swipe: true,
+        touchMove: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
